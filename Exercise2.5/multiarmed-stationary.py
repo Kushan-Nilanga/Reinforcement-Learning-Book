d98df1d10361ebaa75e0979f51df169ed43f852e
Kushan-Nilanga <dknathalage@gmail.com>
2020-10-02 14:57:17 +1000

    comments

--- Exercise2.5/multiarmed-stationary.py
+++ Exercise2.5/multiarmed-stationary.py
@@ -12,9 +12,9 @@ e = 0.005
 
 N = [0 for i in range(10)]
 
+# create bandit
 banditinit = numpy.random.randn(10, 2)+2
 bandit = numpy.prod(banditinit, axis=1)
-print(bandit)
 
 print(f"arms {arms}")
 print(f"Q {Q}")
@@ -22,21 +22,30 @@ print(f"N {N}")
 
 mse = []
 
-# while(True):
+# run for 10000 iterations
 for i in range(10000):
+
+    # getting picking the highest prob action
     id = numpy.argmax(Q)
 
     # calculating the probs
     probs = numpy.ones_like(Q) * e
     probs[id] = 1 - e
     probs = probs/probs.sum()
+
+    # selecting action
     action = numpy.random.choice(arms, p=probs)
 
+    # sampling reward from the bandit
     reward = numpy.random.normal(bandit[action])
+    
     N[action] += 1
+    
+    # update Q table
     Q[action] += 1/N[action] * (reward-Q[action])
+    
+    # calculate mse
     mse.append(0.1 * ((bandit - Q)**2).sum())
-    # placeholder_bandit
 
 plt.plot(mse)
 plt.show()
